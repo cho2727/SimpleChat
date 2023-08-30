0ddab6de6c6fa5a053a7a95eaf1e40070b2c98f9
조신연 <choshinyoun@naver.com>
2023-08-30 18:04:13 +0900

    project comp

---
+++ SimpleChat/Hubs/SimpleChat.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SimpleChat.Hubs;
+
+public class SimpleChat : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
--- SimpleChat/Program.cs
+++ SimpleChat/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.UseHttpsRedirection();
+//app.UseDefaultFiles();
+//app.UseStaticFiles();
 
+app.UseStaticFiles();
+//app.UseRouting();
+//app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
+app.MapHub<SimpleChat.Hubs.SimpleChat>("/simplechat");
+app.MapGet("/hello", () => "Hello World!");
 app.Run();
